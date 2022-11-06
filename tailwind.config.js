00c5b0b61d57b9091b2c518278b09a78c5698a1e
3918-ovhi barua <ovhibarua1992@gmail.com>
2022-11-06 21:00:28 +0600

    notes layouts done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
     extend: { 
+      dropShadow: {
+        'noteShade': '8px 8px 0px #DBDBDB'
+      },
       fontFamily: {
         'inter' : 'Inter',
       },
